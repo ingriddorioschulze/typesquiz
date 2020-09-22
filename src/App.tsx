ad31eaee6b61a73a5923b1130954f3bc4d5ed791
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2020-09-22 23:15:06 +0200

    Add styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 import styled, { ThemeProvider } from 'styled-components'
 import Loader from 'react-loader-spinner'
 
-import { fetchQuizQuestions, Difficulty, QuestionState } from './API'
-
+import theme from './theme'
 import QuestionCard from './QuestionCard'
+import GlobalStyles from './GlobalStyles'
+import { fetchQuizQuestions, Difficulty, QuestionState } from './API'
 
 export type AnswerObject = {
   question: string
@@ -13,7 +14,75 @@ export type AnswerObject = {
   correctAnswer: string
 }
 
-const TOTAL_QUESTIONS = 10
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #eec170;
+  width: 100%;
+  height: 100vh;
+
+  .title {
+    font-size: 80px;
+    font-family: 'Cabin Sketch';
+    padding: 30px;
+    color: #353535;
+  }
+
+  .start-button {
+    font-family: 'Amatic SC';
+    cursor: pointer;
+    border: none;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    color: #353535;
+    background-color: transparent;
+    font-size: 35px;
+    font-weight: bold;
+    padding: 20px;
+  }
+
+  .blinking {
+    animation: blinkingText 1.2s infinite;
+  }
+
+  @keyframes blinkingText {
+    0% {
+      color: #000;
+    }
+    49% {
+      color: #000;
+    }
+    60% {
+      color: transparent;
+    }
+    99% {
+      color: transparent;
+    }
+    100% {
+      color: #000;
+    }
+  }
+
+  .next-button {
+    font-family: 'Amatic SC';
+    cursor: pointer;
+    border: none;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    color: #353535;
+    background-color: transparent;
+    font-size: 35px;
+    font-weight: bold;
+  }
+
+  .score {
+    font-size: 30px;
+    font-family: 'Amatic SC';
+    font-weight: bold;
+    color: #353535;
+  }
+`
 
 function App() {
   const [loading, setLoading] = useState(false)
@@ -22,6 +91,7 @@ function App() {
   const [userAnswers, setUsersAnswers] = useState<AnswerObject[]>([])
   const [score, setScore] = useState(0)
   const [gameOver, setGameOver] = useState(true)
+  const TOTAL_QUESTIONS = 10
 
   const startQuiz = async () => {
     setLoading(true)
@@ -63,38 +133,45 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1>Typesquiz</h1>
-      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-        <button onClick={startQuiz}>Start</button>
-      ) : null}
-      {!gameOver ? <p>Score:</p> : null}
-      {loading ? (
-        <Loader
-          type="ThreeDots"
-          color="#fca311"
-          height={100}
-          width={100}
-          timeout={3000}
-        />
-      ) : null}
-      {!loading && !gameOver && (
-        <QuestionCard
-          questionNumber={number + 1}
-          totalQuestions={TOTAL_QUESTIONS}
-          question={questions[number].question}
-          answers={questions[number].answers}
-          userAnswer={userAnswers ? userAnswers[number] : undefined}
-          callback={checkAnswer}
-        />
-      )}
-      {!gameOver &&
-      !loading &&
-      userAnswers.length === number + 1 &&
-      number !== TOTAL_QUESTIONS - 1 ? (
-        <button onClick={nextQuestion}>Next Question</button>
-      ) : null}
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AppWrapper>
+        <h1 className="title">Typesquiz</h1>
+        {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button className="start-button blinking" onClick={startQuiz}>
+            Start
+          </button>
+        ) : null}
+        {!gameOver ? <p className="score">Score: {score}</p> : null}
+        {loading ? (
+          <Loader
+            type="ThreeDots"
+            color="#b9375e"
+            height={100}
+            width={100}
+            timeout={3000}
+          />
+        ) : null}
+        {!loading && !gameOver && (
+          <QuestionCard
+            questionNumber={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+        {!gameOver &&
+        !loading &&
+        userAnswers.length === number + 1 &&
+        number !== TOTAL_QUESTIONS - 1 ? (
+          <button className="next-button" onClick={nextQuestion}>
+            Next Question
+          </button>
+        ) : null}
+      </AppWrapper>
+    </ThemeProvider>
   )
 }
 
--- src/QuestionCard.tsx
+++ src/QuestionCard.tsx
@@ -1,5 +1,64 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import { AnswerObject } from './App'
+
+const QuestionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  margin: 45px;
+
+  .question-name {
+    font-size: 35px;
+    font-family: 'Amatic SC';
+    color: #353535;
+    font-weight: bold;
+    padding: 10px;
+  }
+
+  .question {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 30px;
+    font-family: 'Amatic SC';
+    color: #353535;
+    padding: 10px;
+  }
+`
+
+type ButtonWrapperProps = {
+  correct: boolean
+  userClicked: boolean
+}
+
+const ButtonWrapper = styled.div<ButtonWrapperProps>`
+  .answer {
+    font-family: 'Amatic SC';
+    cursor: pointer;
+    border: none;
+    margin: 5px;
+    padding: 5px;
+    width: 20vw;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    color: #353535;
+    background-color: ${({ correct, userClicked }) =>
+      correct
+        ? '#2b9348'
+        : !correct && userClicked
+        ? '#f94144'
+        : 'transparent'};
+    font-size: 25px;
+  }
+
+  .answer:hover {
+    font-weight: bold;
+    text-decoration: underline;
+  }
+`
 
 type QuestionsProps = {
   question: string
@@ -19,25 +78,30 @@ function QuestionCard({
   totalQuestions,
 }: QuestionsProps) {
   return (
-    <div>
-      <p>
+    <QuestionWrapper>
+      <p className="question-name">
         Question: {questionNumber} / {totalQuestions}
       </p>
-      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <p className="question" dangerouslySetInnerHTML={{ __html: question }} />
       <div>
         {answers.map((answer) => (
-          <div key={answer}>
+          <ButtonWrapper
+            key={answer}
+            correct={userAnswer?.correctAnswer === answer}
+            userClicked={userAnswer?.answer === answer}
+          >
             <button
+              className="answer"
               disabled={userAnswer ? true : false}
               onClick={callback}
               value={answer}
             >
               <span dangerouslySetInnerHTML={{ __html: answer }} />
             </button>
-          </div>
+          </ButtonWrapper>
         ))}
       </div>
-    </div>
+    </QuestionWrapper>
   )
 }
 
